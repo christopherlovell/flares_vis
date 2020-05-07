706287a1a384cb92eab5ff25a083f39e00ea1835
Will Roper <wjr21@sussex.ac.uk>
2020-05-07 17:52:29 +0100

    added flythrough animation scripts

---
+++ flythrough_animation_frame_adjust.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --ntasks 1 # The number of cores you need...
+#SBATCH --array=1-1000
+#SBATCH --cpus-per-task=8
+#SBATCH -J FLARES-pysphv #Give it something meaningful.
+#SBATCH -o logs/output_flythrough.%J.out
+#SBATCH -e logs/error_flythrough.%J.err
+#SBATCH -p cosma7 #or some other partition, e.g. cosma, cosma6, etc.
+#SBATCH -A dp004
+#SBATCH --exclusive
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=wjr21@sussex.ac.uk #PLEASE PUT YOUR EMAIL ADDRESS HERE (without the <>)
+
+# Run the job from the following directory - change this to point to your own personal space on /lustre
+cd /cosma7/data/dp004/dc-rope1/FLARES/flares
+
+module purge
+#load the modules used to build your program.
+module load pythonconda3/4.5.4
+
+source activate flares-env
+
+i=$(($SLURM_ARRAY_TASK_ID - 1 + 1000))
+
+# Run the program
+./flythrough_animation_frame_adjust.py $i
+
+source deactivate
+
+echo "Job done, info follows..."
+sacct -j $SLURM_JOBID --format=JobID,JobName,Partition,MaxRSS,Elapsed,ExitCode
+exit
+
+
